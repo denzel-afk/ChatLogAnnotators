06c79e55eb91bc2d324fbe52e91035c472ad335f
Denzel Elden Wijaya <denzeledanbanget@gmail.com>
2025-01-10 04:34:56 +0800

    "chore: eslint fixtures"

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import type { Metadata } from "next";
 import "../globals.css";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
